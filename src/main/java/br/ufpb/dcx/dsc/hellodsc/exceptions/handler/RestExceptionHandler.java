ddc1694589970f950a9d06f03fdddd5f4eab3fe6
Raquel Lopes <raquel@computacao.ufcg.edu.br>
2023-06-27 12:15:45 -0300

    First version

--- src/main/java/br/ufpb/dcx/dsc/hellodsc/exceptions/handler/RestExceptionHandler.java
+++ src/main/java/br/ufpb/dcx/dsc/hellodsc/exceptions/handler/RestExceptionHandler.java
@@ -1,7 +1,7 @@
 package br.ufpb.dcx.dsc.hellodsc.exceptions.handler;
 
 import br.ufpb.dcx.dsc.hellodsc.exceptions.StudentNotFoundException;
-import br.ufpb.dcx.hellos.exceptions.model.ApiError;
+import br.ufpb.dcx.dsc.hellodsc.exceptions.model.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
