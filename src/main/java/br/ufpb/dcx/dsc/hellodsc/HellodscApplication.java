9a483d1cabe58a502c69ab2989fd4bb86d530ea7
Raquel Lopes <raquel@computacao.ufcg.edu.br>
2023-06-27 11:26:46 -0300

    Initial commit

---
+++ src/main/java/br/ufpb/dcx/dsc/hellodsc/HellodscApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.dsc.hellodsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellodscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellodscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufpb/dcx/dsc/hellodsc/HellodscApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.dsc.hellodsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellodscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
